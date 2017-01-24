82bf8f2d01f5be02fced685ac44aeaa1b2a6618d
brandon <bcooper085@gmail.com>
2017-01-23 19:56:38 -0800

    add address constructor and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -11,24 +11,48 @@ function Address(street, city, state) {
   this.state = state;
 }
 
-var lisa = new Contact("Lisa", "Simpson");
-var home = new Address("101 S First St.", "Tacoma", "WA");
-
-lisa.addresses.push(home);
-
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+Address.prototype.fullAddress = function() {
+  return this.street + " " + this.city + " " + this.state;
+}
+
 // user interface logic
 $(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+  $('#add-address').click(function() {
+    $('#new-addresses').append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
+
+  $("form#new-contact").submit(function() {
     event.preventDefault();
 
     var inputFirstName = $("input#new-first-name").val();
     var inputLastName = $("input#new-last-name").val();
-
     var newContact = new Contact(inputFirstName, inputLastName);
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+    });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
@@ -37,9 +61,15 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
   });
 });
